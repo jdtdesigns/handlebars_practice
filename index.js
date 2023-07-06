d68ba1758a522a2ef0875888497b8cf5dbbf026c
JD Tadlock <jt7903@gmail.com>
2023-07-06 14:57:26 -0400

    finished up

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -6,8 +7,38 @@ const connection = mysql.createConnection({
   database: 'books_db'
 });
 
-connection.query('SELECT * FROM biographies', (err, data) => {
-  if (err) return console.log(err);
+function printAllBios() {
+  connection.query('SELECT * FROM biographies', (err, data) => {
+    if (err) return console.log(err);
 
-  console.log(data);
+    console.log(data);
+    connection.end();
+  });
+}
+
+inquirer.prompt([
+  {
+    name: 'title',
+    message: 'Please provide a title for the biography.'
+  },
+  {
+    name: 'author',
+    message: 'Please enter the author name.'
+  }
+]).then((answerObj) => {
+  // Query our db and insert a new row into the biographies table
+  const sql = 'INSERT INTO biographies (name, author) VALUES (?, ?)';
+
+  connection.query(sql, [answerObj.title, answerObj.author], (err, result) => {
+    if (err) return console.log(err);
+    console.log('New bio with id of %s added successfully!\n', result.insertId);
+
+    printAllBios();
+  });
 });
+
+
+
+
+
+
