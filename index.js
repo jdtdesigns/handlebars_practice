09b22a0f5a7f7c4f2d5d5e08c5077208ea513e38
JD Tadlock <jt7903@gmail.com>
2023-07-06 15:21:22 -0400

    switched to server

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
+const express = require('express');
 const mysql = require('mysql2');
-const inquirer = require('inquirer');
-const isProduction = process.env.NODE_ENV === 'production';
+
+const app = express();
+const PORT = process.env.PORT || 3333;
+
+const isProduction = process.env.PORT;
 const connectionString = 'mysql://ti3an634a6rwkaha:wlyr6sfhsoa3u4f7@uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/jlepkhxiycgsz5i2';
 
 const connection = mysql.createConnection(isProduction ? connectionString : {
   host: 'localhost',
   user: 'root',
-  database: 'books_db'
+  database: 'books_db',
+  multipleStatements: true
 });
 
-function printAllBios() {
-  connection.query('SELECT * FROM biographies', (err, data) => {
-    if (err) return console.log(err);
+app.get('/', (clientReq, serverRes) => {
+  const tableSQL = `
+  DROP TABLE IF EXISTS biographies;
 
-    console.log(data);
-    connection.end();
-  });
-}
+  CREATE TABLE biographies (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(250),
+    author VARCHAR(250),
+    PRIMARY KEY(id)
+  );
 
-inquirer.prompt([
-  {
-    name: 'title',
-    message: 'Please provide a title for the biography.'
-  },
-  {
-    name: 'author',
-    message: 'Please enter the author name.'
-  }
-]).then((answerObj) => {
-  // Query our db and insert a new row into the biographies table
-  const sql = 'INSERT INTO biographies (name, author) VALUES (?, ?)';
+  INSERT INTO biographies (name, author) VALUES 
+    ('Titanic', 'JD'),
+    ('Another', 'JD'),
+    ('One More', 'JD');
+  `;
 
-  connection.query(sql, [answerObj.title, answerObj.author], (err, result) => {
-    if (err) return console.log(err);
-    console.log('New bio with id of %s added successfully!\n', result.insertId);
+  connection.query(tableSQL, (err) => {
+    if (err) throw err;
 
-    printAllBios();
+    connection.query('SELECT * FROM biographies', (err, data) => {
+      if (err) return console.log(err);
+
+      serverRes.send(data);
+    });
   });
 });
+
+app.listen(PORT, () => console.log('Server started...'));
+
 
 
 
