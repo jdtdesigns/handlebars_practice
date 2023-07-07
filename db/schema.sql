a098c6ace3e01cb6c75df703e9a5afcec07f530c
JD Tadlock <jt7903@gmail.com>
2023-07-07 13:18:43 -0400

    finished foreign basics

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,45 @@
--- Drop or delete the database to reset the data - erase all data/tables
-DROP DATABASE IF EXISTS books_db;
--- Create our books_db database
-CREATE DATABASE books_db;
+DROP DATABASE IF EXISTS project_tracker_db;
 
-USE books_db;
+CREATE DATABASE project_tracker_db;
 
-CREATE TABLE biographies (
-  id INT AUTO_INCREMENT,
-  name VARCHAR(250),
-  author VARCHAR(250),
-  PRIMARY KEY(id)
+USE project_tracker_db;
+
+CREATE TABLE students (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(250) NOT NULL,
+  last_name VARCHAR(250) NOT NULL
 );
 
-INSERT INTO biographies (name, author) VALUES 
-  ('Titanic', 'JD'),
-  ('Another', 'JD'),
-  ('One More', 'JD');
+CREATE TABLE projects (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  project_name VARCHAR(250) NOT NULL,
+  completed BOOLEAN DEFAULT false,
+  student_id INT NOT NULL,
+  FOREIGN KEY (student_id) REFERENCES students(id)
+    ON DELETE CASCADE
+);
+
+INSERT INTO students (first_name, last_name) VALUES 
+  ('John', 'Jones'),
+  ('Jacob', 'Marley'),
+  ('Jingle', 'Bells'),
+  ('Heimer', 'Schmidt'),
+  ('Minnie', 'Mouse'),
+  ('Mickey', 'Mouse');
+
+INSERT INTO projects (project_name, student_id) VALUES
+  ('Project A', 1),
+  ('Project B', 1),
+  ('Project C', 3),
+  ('Project D', 4),
+  ('Project E', 5);
+
+-- SELECT * FROM students;
+-- SELECT * FROM projects;
+
+SELECT
+  CONCAT(first_name, ' ', last_name) AS student_name,
+  project_name,
+  completed
+    FROM students
+    JOIN projects ON students.id = projects.student_id;
--- index.js
+++ index.js
@@ -4,49 +4,14 @@ const mysql = require('mysql2');
 const app = express();
 const PORT = process.env.PORT || 3333;
 
-const isProduction = process.env.PORT;
-
-const cloudConnection = {
-  host: 'uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user: 'ti3an634a6rwkaha',
-  password: 'wlyr6sfhsoa3u4f7',
-  database: 'jlepkhxiycgsz5i2',
-  multipleStatements: true
-};
-
-const connection = mysql.createConnection(isProduction ? cloudConnection : {
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  database: 'books_db',
-  multipleStatements: true
+  database: 'project_tracker_db',
 });
 
 app.get('/', (clientReq, serverRes) => {
-  const tableSQL = `
-  DROP TABLE IF EXISTS biographies;
 
-  CREATE TABLE biographies (
-    id INT AUTO_INCREMENT,
-    name VARCHAR(250),
-    author VARCHAR(250),
-    PRIMARY KEY(id)
-  );
-
-  INSERT INTO biographies (name, author) VALUES 
-    ('Titanic', 'JD'),
-    ('Another', 'JD'),
-    ('One More', 'JD');
-  `;
-
-  connection.query(tableSQL, (err) => {
-    if (err) throw err;
-
-    connection.query('SELECT * FROM biographies', (err, data) => {
-      if (err) return console.log(err);
-
-      serverRes.send(data);
-    });
-  });
 });
 
 app.listen(PORT, () => console.log('Server started...'));
