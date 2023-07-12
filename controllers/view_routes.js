ab5c5e7c41c8f0ffb7fa290247aa3ea71483d0da
JD Tadlock <jt7903@gmail.com>
2023-07-12 13:06:17 -0400

    cleaned up files and added some base styles

--- controllers/view_routes.js
+++ controllers/view_routes.js
@@ -1,24 +1,13 @@
 const router = require('express').Router();
-const path = require('path');
 
 router.get('/', (req, res) => {
   res.render('index', {
-    users: [
-      {
-        email: 'jd@test.com'
-      },
-      {
-        email: 'bob@test.com'
-      },
-      {
-        email: 'jane@test.com'
-      }
-    ]
+
   });
 });
 
-router.get('/about', (req, res) => {
-  res.render('about');
+router.get('/register', (req, res) => {
+  res.render('register');
 })
 
 module.exports = router;
